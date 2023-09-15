22f5408cfa0b1cf9e20fd57e01dfb191bd52f739
Zimuzo Akanne <127262688+Zim67@users.noreply.github.com>
2023-09-15 02:20:04 -0230

    Add files via upload

---
+++ TestDate.java
@@ -0,0 +1,14 @@
+public class TestDate {
+    public static void main(String[] args) {
+
+        //Create a date object using the date class
+        Date date = new Date(14, 9, 2023);
+
+        System.out.println("\n");
+        System.out.println("Date");
+        System.out.println("----");
+
+        //print the Date object (d1) using the tostring method
+        System.out.println(date.toString());
+    }
+}
